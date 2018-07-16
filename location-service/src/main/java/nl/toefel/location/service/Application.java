d44e2afdf6d22cac604ff134d47ce5204d5541a1
toefel <toefel18@gmail.com>
2018-07-16 15:42:30 +0200

    Added spring retry, createClob exception is not printed anymore, switched to undertow

--- location-service/src/main/java/nl/toefel/location/service/Application.java
+++ location-service/src/main/java/nl/toefel/location/service/Application.java
@@ -2,24 +2,48 @@ package nl.toefel.location.service;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+
+import java.net.ConnectException;
 
 @SpringBootApplication
+@EnableRetry
 public class Application {
+
+    public static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    //https://docs.spring.io/spring-boot/docs/current/reference/html/howto-data-access.html#howto-configure-a-datasource
-    //https://docs.spring.io/spring-boot/docs/current/reference/html/howto-database-initialization.html
+    @Bean
+    public RetryTemplate retryTemplate() {
+        RetryTemplate retryTemplate = new RetryTemplate();
 
-    //TODO disable createClob check https://vkuzel.com/spring-boot-jpa-hibernate-atomikos-postgresql-exception
-    //TODO http://fabiomaffioletti.me/blog/2014/04/15/distributed-transactions-multiple-databases-spring-boot-spring-data-jpa-atomikos/
+        ExponentialBackOffPolicy exponentialBackOffPolicy = new ExponentialBackOffPolicy();
+        exponentialBackOffPolicy.setInitialInterval(500);
+        exponentialBackOffPolicy.setMaxInterval(1000);
+        exponentialBackOffPolicy.setMultiplier(1.8);
+        retryTemplate.setBackOffPolicy(exponentialBackOffPolicy);
+
+        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
+        simpleRetryPolicy.setMaxAttempts(3);
+        retryTemplate.setRetryPolicy(simpleRetryPolicy);
+
+        return retryTemplate;
+    }
 
     @Bean
+    @Retryable(value= {ConnectException.class}, maxAttempts = 500)
     public HikariDataSource dataSource() {
         Config cfg = Config.fromEnvironment();
 
